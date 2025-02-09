8c7867c11a4ab3579fa55c662f300c9f58110b82
Aashish Kumar <75846368+aashish350@users.noreply.github.com>
2025-02-09 14:47:55 +0530

    Delete JobPortal-Using-SpringBoot-ReactJS-master directory
    i  uploaded wrong file

--- JobPortal-Using-SpringBoot-ReactJS-master/src/main/java/com/pushkar/SpringBoot_REST/repository/UserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.pushkar.SpringBoot_REST.repository;
-
-import com.pushkar.SpringBoot_REST.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepo extends JpaRepository<User,Integer> {
-    User findByUsername(String username);
-}
